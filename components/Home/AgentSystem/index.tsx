3a9cfade471d8972b30c33664a4db60a6c604a44
vu-hoang <hoangvu15499@gmail.com>
2022-10-05 14:07:25 +0700

    Add: agent system

--- components/Home/AgentSystem/index.tsx
+++ components/Home/AgentSystem/index.tsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
-import Image from 'next/image'
+import React, { FC } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
-import styles from './styles.module.scss';
-import ImageMachine from '~/assets/image/bg-tri-nam-3.png';
+import styles from "./styles.module.scss";
+import ImageMachine from "~/assets/image/bg-tri-nam-3.png";
+import CardItem from "./CardItem";
 
-const AgentSystem = () => {
+import dataAgentSystem from "../data.json";
+
+const AgentSystem: FC = () => {
   return (
     <Container>
       <Row className={styles.agentSystem}>
         <h2 className={styles.titleAgentSystem}>HỆ THỐNG ĐẠI LÝ</h2>
 
-        <Col xs={12} md={6} className={styles.imageMachine} >
+        <Col xs={12} md={6} className={styles.imageMachine}>
           <div className={styles.image} />
         </Col>
         <Col xs={12} md={6} className={styles.content}>
-          <Row>
-            <Col xs={3} className={styles.imageContent}>
-              <div className={styles.image} />
-            </Col>
-
-            <Col xs={9} className={styles.productDescription}>
-              <h4 className={styles.title}>MÁY LASER TRỊ NÁM</h4>
-              <p className={styles.description}>
-                Các dòng máu điều trị nám tại HPMED được nhiều chuuên gia đồu ngành da liễu và khách hàng đónh gió cao về chốt lượng.
-              </p>
-            </Col>
-          </Row>
+          {
+            dataAgentSystem.agentSystem.map((item)=>(
+              <CardItem 
+                key={item.title}
+                title={item.title}
+                description={item.description}
+              />
+            ))
+          }
         </Col>
       </Row>
     </Container>
-  )
-}
+  );
+};
 
 export default AgentSystem;
