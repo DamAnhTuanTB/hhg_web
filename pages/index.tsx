296eee2c510bc53080922129939746bc36dee46e
Alan Nguyen <alan.nguyen@setel.com>
2022-10-06 17:37:11 +0700

    Done introduction and services

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Slider from "~/components/Home/Slider";
 import NewsHomePage from "~/components/Home/NewsHomePage";
 import AgentSystem from "~/components/Home/AgentSystem";
 import Partner from "~/components/Home/Partner";
+import Introduction from "~/components/Home/Introduction";
+import Services from "~/components/Home/Services";
 
 const Home = () => {
   return (
     <>
       <Slider />
-
+      <Introduction />
+      <Services />
       <AgentSystem />
       <Partner />
       <NewsHomePage />
